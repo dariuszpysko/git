c8a56911b40466d78e9c40068450609f0cb98030
Dariusz Pysko <dariusz.pysko@gmail.com>
2022-09-20 14:35:53 +0200

    fffff

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 const handler = (request, response) => {
    // console.log("welcome user")
-    response.end("nowa stronaklkjljklkj47657658762");
+    response.end("nowa stronaklkjlbhgfhgfhfjklkj47657658762");
 
 };
 const server = http.createServer(handler);
